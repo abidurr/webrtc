12a6157ef80d5be36b1271dd9d17fa665651377e
abidurr <42abid@gmail.com>
2020-08-30 18:58:50 -0400

    Server set up

--- server.js
+++ server.js
@@ -11,13 +11,18 @@ app.get("/", (req, res) => {
     res.redirect(`/${uuidV4()}`);
 });
 
-app.get(":/room", (req, res) => {
+app.get("/:room", (req, res) => {
     res.render("room", { roomId: req.params.room });
 });
 
 io.on("connection", (socket) => {
     socket.on("join-room", (roomId, userId) => {
-        console.log(roomId, userId);
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit("user-connected", userId);
+
+        socket.on("disconnect", () => {
+            socket.to(roomId).broadcast.emit("user-disconnected", userId);
+        });
     });
 });
 
